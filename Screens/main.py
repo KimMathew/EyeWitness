5ec423d0dbb38e382f907ef8a8f7b4262fa80210
Kim Mathew Bautista <kimmathewbautista11@gmail.com>
2023-11-13 09:54:01 +0800

    Added Fonts

--- Screens/main.py
+++ Screens/main.py
@@ -11,17 +11,18 @@ Window.size = (360, 600)
 class MyApp(MDApp):
     def build(self):
         screen_manager = MDScreenManager()
+        screen_manager.add_widget(Builder.load_file("Screens\HomeScreen\homescreen_admin.kv"))
         screen_manager.add_widget(Builder.load_file("Screens\LoginScreen\main.kv"))
         screen_manager.add_widget(Builder.load_file("Screens\HomeScreen\screenreport.kv"))
         screen_manager.add_widget(Builder.load_file("Screens\HomeScreen\homescreen.kv"))
         screen_manager.add_widget(Builder.load_file("Screens\LoginScreen\login.kv"))
         screen_manager.add_widget(Builder.load_file("Screens\LoginScreen\signup.kv"))
         screen_manager.add_widget(Builder.load_file("Screens\HomeScreen\sosscreen.kv"))
-        screen_manager.add_widget(Builder.load_file("Screens\HomeScreen\homescreen_admin.kv"))
+        
         return screen_manager
 
 if __name__ == "__main__":
-    LabelBase.register(name = "MPoppins", fn_regular="C:\\Fonts\\Poppins\\Poppins-Medium.ttf")
-    LabelBase.register(name = "BPoppins", fn_regular="C:\\Fonts\\Poppins\\Poppins-SemiBold.ttf")
+    LabelBase.register(name = "MPoppins", fn_regular="Screens\\Assets\\Poppins\\Poppins-Medium.ttf")
+    LabelBase.register(name = "BPoppins", fn_regular="Screens\\Assets\\Poppins\\Poppins-SemiBold.ttf")
 
     MyApp().run()
